38c4b91eb29f351291e921cecd85344da007294c
rolly <rollrodrig@gmail.com>
2019-07-06 16:54:06 -0500

    fixed some names

--- dist/NavBarBuilder.js
+++ dist/NavBarBuilder.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const FileInfo_1 = __importDefault(require("./FileInfo"));
 class NavBarBuilder {
     static create(items) {
-        let html = `<li><a href="">Api</a></li>`;
+        let html = `<li><a href="">Routes</a></li>`;
         items.map((item) => {
             let name = FileInfo_1.default.get(item);
             html += `<li><a href="${name.href}">${name.name}</a></li>`;
--- src/NavBarBuilder.ts
+++ src/NavBarBuilder.ts
@@ -1,7 +1,7 @@
 import FileInfo, { TFileInfo } from "./FileInfo";
 class NavBarBuilder {
     static create(items:string[]){
-        let html:string = `<li><a href="">Api</a></li>`;
+        let html:string = `<li><a href="">Routes</a></li>`;
         items.map((item:string)=>{
             let name:TFileInfo  = FileInfo.get(item);
             html +=`<li><a href="${name.href}">${name.name}</a></li>`;
